77708715ce36efa54b1ccb667f0896a12a4c7d34
xinz <joy2everyone.z@gmail.com>
2019-12-05 16:29:26 +0800

    Version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAliyunAmqp.MixProject do
   def project do
     [
       app: :ex_aliyun_amqp,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
